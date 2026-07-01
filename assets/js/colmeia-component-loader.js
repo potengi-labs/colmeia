15989797cb017f1df42756410beeb700115c5070
Potengi Labs <ladulcelara@gmail.com>
2026-07-01 00:12:33 -0300

    Update colmeia-component-loader.js

--- assets/js/colmeia-component-loader.js
+++ assets/js/colmeia-component-loader.js
@@ -63,17 +63,17 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     loadComponent(
       "header-container",
-      "assets/components/vital-header.html"
+      "assets/components/colmeia-header.html"
     ),
 
     loadComponent(
       "sidebar-container",
-      "assets/components/vital-sidebar.html"
+      "assets/components/colmeia-sidebar.html"
     ),
     
     loadComponent( 
       "footer-container", 
-      "assets/components/vital-footer.html" 
+      "assets/components/colmeia-footer.html" 
     )
 
   ]);
